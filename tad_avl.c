cec9487a4068d2952442a299860dd02c3c9e5ca0
gabriel-ste <gabriel.stepien.eu@gmail.com>
2016-11-10 20:15:45 -0200

    creating avl tad
    first commit

---
+++ tad_avl.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tNodoA
+{
+    int info;
+    struct tNodoA *esq;
+    struct tNodoA *dir;
+};
+typedef struct tNodoA pNodoA;
+
+pNodoA* rotacao_esquerda(pNodoA *pt)
+{
+	pNodoA *ptu;
+	ptu = pt->dir;
+	pt->dir = ptu->esq;
+	ptu->esq = pt;
+	pt = ptu;
+return pt;
+} 
