a4118868af1f151c30c6de19a5e40361dd61772b
gabriel-ste <gabriel.stepien.eu@gmail.com>
2016-11-10 21:05:41 -0200

    v1.1
    Funções de inserção em uma AVL foram implementadas.

--- tad_avl.c
+++ tad_avl.c
@@ -1,21 +1,282 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct tNodoA
+typedef int TipoInfo;
+
+struct TNodoA
 {
-    int info;
-    struct tNodoA *esq;
-    struct tNodoA *dir;
+    TipoInfo info;
+    int FB;
+    struct TNodoA *esq;
+    struct TNodoA *dir;
 };
-typedef struct tNodoA pNodoA;
+typedef struct TNodoA pNodoA;
+
+/*
+    This function is going to be used to initialize the tree
+*/
+pNodoA* inicializa(void)
+{
+    return NULL;
+}
+
+
+/*
+    Code that applies a simple rotation left to the node
+    *function based in the classes presentations
+*/
 
 pNodoA* rotacao_esquerda(pNodoA *pt)
 {
-	pNodoA *ptu;
-	test
-	ptu = pt->dir;
-	pt->dir = ptu->esq;
-	ptu->esq = pt;
-	pt = ptu;
-return pt;
-} 
+    pNodoA *ptu;
+    ptu = pt->dir;
+    pt->dir = ptu->esq;
+    ptu->esq = pt;
+    pt->FB = 0;
+    pt = ptu;
+    return pt;
+}
+
+/*
+    Code that applies a simple right rotation to the node
+    *function based in the classes presentations
+*/
+pNodoA* rotacao_direita(pNodoA* pt)
+{
+    pNodoA *ptu;
+    ptu = pt->esq;
+    pt->esq = ptu->dir;
+    ptu->dir = pt;
+    pt->FB = 0;
+    pt = ptu;
+    return pt;
+}
+
+/*
+    Code that applies a double right rotation to the node
+    *function based in the classes presentations
+*/
+pNodoA* rotacao_dupla_direita (pNodoA* pt)
+{
+    pNodoA *ptu, *ptv;
+    ptu = pt->esq;
+    ptv = ptu->dir;
+    ptu->dir = ptv->esq;
+    ptv->esq = ptu;
+    pt->esq = ptv->dir;
+    ptv->dir = pt;
+    if (ptv->FB == 1) pt->FB = -1;
+    else pt->FB = 0;
+    if (ptv->FB == -1) ptu->FB = 1;
+    else ptu->FB = 0;
+    pt = ptv;
+    return pt;
+}
+
+/*
+    Code that applies a double left rotation to the node
+    *function based in the classes presentations
+*/
+pNodoA* rotacao_dupla_esquerda (pNodoA *pt)
+{
+    pNodoA *ptu, *ptv;
+    ptu = pt->dir;
+    ptv = ptu->esq;
+    ptu->esq = ptv->dir;
+    ptv->dir = ptu;
+    pt->dir = ptv->esq;
+    ptv->esq = pt;
+    if (ptv->FB == -1) pt->FB = 1;
+    else pt->FB = 0;
+    if (ptv->FB == 1) ptu->FB = -1;
+    else ptu->FB = 0;
+    pt = ptv;
+    return pt;
+}
+
+pNodoA* Caso1 (pNodoA *a, int *ok)
+{
+    pNodoA *ptu;
+    ptu = a->esq;
+    if (ptu->FB == 1)
+    {
+        printf("fazendo rotacao direita em %d\n",a->info);
+        a = rotacao_direita(a);
+    }
+    else
+    {
+        printf("fazendo rotacao dupla direita em %d\n",a->info);
+        a = rotacao_dupla_direita(a);
+    }
+    a->FB = 0;
+    *ok = 0;
+    return a;
+}
+
+pNodoA* Caso2 (pNodoA *a, int *ok)
+{
+    pNodoA *ptu;
+    ptu = a->dir;
+    if (ptu->FB == -1)
+    {
+        printf("fazendo rotacao esquerda em %d\n",a->info);
+        a=rotacao_esquerda(a);
+    }
+    else
+    {
+        printf("fazendo rotacao dupla esquerda em %d\n",a->info);
+        a=rotacao_dupla_esquerda(a);
+    }
+    a->FB = 0;
+    *ok = 0;
+    return a;
+}
+
+/*
+    This function is used to insert nodes in to a AVL tree. It ensures that the tree will be with the right factor.
+    *function based in the classes presentations
+*/
+
+pNodoA* InsereAVL (pNodoA *a, TipoInfo x, int *ok)
+{
+    /* Insere nodo em uma árvore AVL, onde A representa a raiz da árvore,
+    x, a chave a ser inserida e h a altura da árvore */
+    if (a == NULL)
+    {
+        a = (pNodoA*) malloc(sizeof(pNodoA));
+        a->info = x;
+        a->esq = NULL;
+        a->dir = NULL;
+        a->FB = 0;
+        *ok = 1;
+    }
+    else if (x < a->info)
+    {
+        a->esq = InsereAVL(a->esq,x,ok);
+        if (*ok)
+        {
+            switch (a->FB)
+            {
+            case -1:
+                a->FB = 0;
+                *ok = 0;
+                break;
+            case 0:
+                a->FB = 1;
+                break;
+            case 1:
+                a = (pNodoA*) Caso1(a,ok);
+                break;
+            }
+        }
+    }
+    else
+    {
+        a->dir = InsereAVL(a->dir,x,ok);
+        if (*ok)
+        {
+            switch (a->FB)
+            {
+            case 1:
+                a->FB = 0;
+                *ok = 0;
+                break;
+            case 0:
+                a->FB = -1;
+                break;
+            case -1:
+                a = (pNodoA *) Caso2(a,ok);
+                break;
+            }
+        }
+    }
+    return a;
+}
+
+/*
+    The following functions are the in the pattern to print nodes of binary trees.
+    *all these functions are based in the classes presentations.
+*/
+void caminhamento_central_esq(pNodoA *a)
+{
+    if (a!= NULL)
+    {
+        caminhamento_central_esq(a->esq);
+        printf("%d ",a->info);
+        caminhamento_central_esq(a->dir);
+    }
+}
+
+void caminhamento_central_dir(pNodoA *a)
+{
+    if (a!= NULL)
+    {
+        caminhamento_central_dir(a->dir);
+        printf("%d ",a->info);
+        caminhamento_central_dir(a->esq);
+    }
+}
+
+void caminhamento_preFixado_esq(pNodoA *a)
+{
+    if (a!= NULL)
+    {
+        printf("%d ",a->info);
+        caminhamento_preFixado_esq(a->esq);
+        caminhamento_preFixado_esq(a->dir);
+    }
+}
+
+void caminhamento_posFixado_esq(pNodoA *a)
+{
+    if (a!= NULL)
+    {
+        caminhamento_posFixado_esq(a->esq);
+        caminhamento_posFixado_esq(a->dir);
+        printf("%d ",a->info);
+    }
+}
+/*
+    This function will print a tree using equals to define the nodes levels.
+            developed in laboratory's class.
+*/
+
+void caminhamento_preFixado_esq_barras(pNodoA *a, int nivel)
+{
+    int i;
+    if (a!= NULL)
+    {
+        for(i=0; i<=nivel; i++)
+            printf("=");
+        printf("%d\n",a->info);
+        nivel ++;
+        caminhamento_preFixado_esq_barras(a->esq,nivel);
+        caminhamento_preFixado_esq_barras(a->dir,nivel);
+    }
+}
+
+
+// end of plot functions
+
+
+int main()
+{
+    int *ok;
+
+    pNodoA *a;
+    a = inicializa();
+    a = InsereAVL(a,10,ok);
+    a = InsereAVL(a,2,ok);
+    a = InsereAVL(a,3,ok);
+    a = InsereAVL(a,11,ok);
+    a = InsereAVL(a,20,ok);
+    a = InsereAVL(a,31,ok);
+    a = InsereAVL(a,58,ok);
+    a = InsereAVL(a,37,ok);
+    a = InsereAVL(a,34,ok);
+    a = InsereAVL(a,25,ok);
+    a = InsereAVL(a,0,ok);
+
+    caminhamento_preFixado_esq_barras(a,0);
+
+}
